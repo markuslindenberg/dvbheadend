cdf00496fb9a6a7a9be70a999b4812bd4a85afa0
Markus Lindenberg <markus@lindenberg.io>
2020-04-11 10:21:47 +0200

    Add F2FS storage partition for tvheadend, automatically resize storage partition

--- board/raspberrypi/post-image.sh
+++ board/raspberrypi/post-image.sh
@@ -11,8 +11,10 @@ CONFIG_TXT="${BOARD_DIR}/config-${BOARD_NAME}.txt"
 cp -f "${CONFIG_TXT}" ${BINARIES_DIR}/rpi-firmware/config.txt
 echo "console=tty1 console=ttyS0,115200" > ${BINARIES_DIR}/rpi-firmware/cmdline.txt
 
-rm -rf "${GENIMAGE_TMP}"
+dd if=/dev/zero of=${BINARIES_DIR}/storage.f2fs bs=1M count=128
+mkfs.f2fs -R 1000:1000 -l storage ${BINARIES_DIR}/storage.f2fs
 
+rm -rf "${GENIMAGE_TMP}"
 genimage                           \
 	--rootpath "${TARGET_DIR}"     \
 	--tmppath "${GENIMAGE_TMP}"    \
