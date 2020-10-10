6c40eca12a52629a9b453d93884750774203019c
Markus Lindenberg <markus@lindenberg.io>
2020-10-10 13:40:21 +0200

    Clean up board config, default disable serial console

--- board/raspberrypi4-64/post-image.sh
+++ board/raspberrypi4-64/post-image.sh
@@ -3,23 +3,19 @@
 set -e
 
 BOARD_DIR="$(dirname $0)"
-BOARD_NAME="$(basename ${BOARD_DIR})"
-GENIMAGE_CFG="${BOARD_DIR}/genimage-${BOARD_NAME}.cfg"
-GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
-CONFIG_TXT="${BOARD_DIR}/config-${BOARD_NAME}.txt"
-
-cp -f "${CONFIG_TXT}" ${BINARIES_DIR}/rpi-firmware/config.txt
-echo "console=tty1 console=ttyS0,115200" > ${BINARIES_DIR}/rpi-firmware/cmdline.txt
+install -D -m 0644 "${BOARD_DIR}/config.txt" "${BINARIES_DIR}/rpi-firmware/config.txt"
+install -D -m 0644 "${BOARD_DIR}/cmdline.txt" "${BINARIES_DIR}/rpi-firmware/cmdline.txt"
 
 dd if=/dev/zero of=${BINARIES_DIR}/storage.f2fs bs=1M count=128
 mkfs.f2fs -R 1000:1000 -l storage ${BINARIES_DIR}/storage.f2fs
 
+GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
 rm -rf "${GENIMAGE_TMP}"
 genimage                           \
 	--rootpath "${TARGET_DIR}"     \
 	--tmppath "${GENIMAGE_TMP}"    \
 	--inputpath "${BINARIES_DIR}"  \
 	--outputpath "${BINARIES_DIR}" \
-	--config "${GENIMAGE_CFG}"
+	--config "${BOARD_DIR}/genimage.cfg"
 
 exit $?
