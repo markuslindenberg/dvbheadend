06310f032d2574196737a4dc72e13d3f4b4c8d7e
Markus Lindenberg <markus@lindenberg.io>
2020-10-10 11:46:16 +0200

    Remove replacing of pam config

--- board/common/post-build.sh
+++ board/common/post-build.sh
@@ -3,10 +3,6 @@
 set -u
 set -e
 
-rm -f ${TARGET_DIR}/etc/pam.d/*
-cp -f ${TARGET_DIR}/usr/share/factory/etc/pam.d/* ${TARGET_DIR}/etc/pam.d/
-rm -rf ${TARGET_DIR}/etc/network
-
 cat << EOF > ${TARGET_DIR}/etc/os-release
 NAME=dvbheadend
 ID=dvbheadend
